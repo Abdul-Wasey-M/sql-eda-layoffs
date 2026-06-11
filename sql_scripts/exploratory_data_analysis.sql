ea181a0673211e7ec825926a4e5624ddd4554642
Abdul-Wasey-M <mdabdulwa3@gmail.com>
2026-06-11 23:15:24 +0530

    Added exploratory analysis of companies industries and countries

--- sql_scripts/exploratory_data_analysis.sql
+++ sql_scripts/exploratory_data_analysis.sql
@@ -1,1 +1,38 @@
-SELECT * FROM world_layoffs.layoffs_staging2;
+-- EXPLORATORY DATA ANALYSIS
+
+SELECT * 
+FROM layoffs_staging2;
+
+-- Section 1: Initial Exploration
+
+SELECT MAX(total_laid_off),MAX(percentage_laid_off)
+FROM layoffs_staging2;
+
+SELECT * 
+FROM layoffs_staging2
+WHERE total_laid_off=12000;
+
+SELECT * 
+FROM layoffs_staging2
+WHERE percentage_laid_off=1
+ORDER BY total_laid_off DESC;
+
+-- Section 2: Company / Industry / Country Analysis
+
+SELECT company, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company
+ORDER BY 2 DESC;
+
+SELECT MIN(`date`), MAX(`date`)
+FROM layoffs_staging2;
+
+SELECT industry, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY industry
+ORDER BY 2 DESC;
+
+SELECT country, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY country
+ORDER BY 2 DESC;
